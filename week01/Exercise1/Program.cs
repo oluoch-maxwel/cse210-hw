0b22829c99b16ea52ed14a690a5d8f0ce1c4b670
oluoch-maxwel <olumax0@gmail.com>
2026-03-06 10:09:46 +0300

    First Exercise added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,19 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    { 
+        Console.WriteLine();
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last Name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {lastName}, {firstName} {lastName}.");
+        string fullName = Console.ReadLine();
+        
+        Console.WriteLine();
     }
 }
