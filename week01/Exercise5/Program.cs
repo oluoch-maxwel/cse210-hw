42025e717b7a717f0377013347298566d5e7be3c
oluoch-maxwel <olumax0@gmail.com>
2026-03-12 21:44:15 +0300

    Resumes project

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,13 +6,37 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
         DisplayWelcome();
+        string name = PromptUserName();
+        int numNumber = PromptUserNumber();
+        int squared = SquareNumber(numNumber);
+        DisplayResult(name, squared);
+
     }
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome Student to CSE210");
     }
-    static string PromptUserName(string userName)
+    static string PromptUserName()
     {
-        Console.WriteLine(userNmae);
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Enter number");
+        string numString = Console.ReadLine();
+        int numNumber = int.Parse(numString);
+        return numNumber;
+    }
+    static int SquareNumber(int numNumber)
+    {
+        int square = numNumber * numNumber;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1. _company = "Microsoft";
+        job1. _jobTitle = "Software Engineer";
+        job1. _startYear = 2020;
+        job1. _endYear = 2026;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2. _company = "Apple";
+        job2. _jobTitle = "CEO";
+        job2. _startYear = 2020;
+        job2. _endYear = 2026;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Oluoch Maxwel";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+        
     }
 }
