350915046dd0f01d7dd61fdb0397f36a056b3731
oluoch-maxwel <olumax0@gmail.com>
2026-03-20 00:08:07 +0300

    Franction Assiggnment Completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Default constructor
+        Fraction function1 = new Fraction();
+
+        // One-parameter constructor
+        Fraction function2 = new Fraction(5);
+
+        // Two-parameter constructor
+        Fraction function3 = new Fraction(2, 3);
+
+        function1.Display();
+        function2.Display();
+        function3.Display();
     }
 }
